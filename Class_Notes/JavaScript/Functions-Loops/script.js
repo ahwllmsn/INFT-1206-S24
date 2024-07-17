6c3a7756361f07a0414c95bfe57b48eb79f2ec8f
Alyssa <alyssahwllmsn@gmail.com>
2024-07-17 17:13:02 -0400

    Loops code-along

---
+++ Class_Notes/JavaScript/Functions-Loops/script.js
@@ -0,0 +1,50 @@
+console.log("Script is running...")
+
+
+// ------------- Loops -------------
+
+// For Loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// While Loop
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(i)
+//     i++;
+// }
+
+// Do While Loop
+
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 10)
+
+    
+// For Of Loop
+
+// let numsArray = [1, 2, 3, 4, 5, 6, 7]
+// for (let num of numsArray) {
+//     console.log(num)
+// }
+
+// For In Loop
+let car = {type: "Hypercar", brand: "Porsche", model: "Spyder"}
+
+for (let key in car) {
+    console.log(key)
+    console.log(car[key])
+}
+
+
+
+
+
+// ------------- Functions -------------
+
+
+
