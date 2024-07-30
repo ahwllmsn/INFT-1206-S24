19316dbc8bc2a1633795516125aee9eb36962ba3
Alyssa <alyssahwllmsn@gmail.com>
2024-07-30 10:54:40 -0400

    Set up looping through images with placeholder values

--- Assignments/Assignment_4/Part_2/main.js
+++ Assignments/Assignment_4/Part_2/main.js
@@ -5,10 +5,24 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFileNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = {
+    pic1: "A closeup of a human eye.",
+    pic2: "A wave-like rock formation.",
+    pic3: "A closeup of white and purple flowers.",
+    pic4: "An Egyptian painting from the Tomb of Tutankhamun.",
+    pic5: "A brown moth with spotted wings on a leaf."
+}
 
 /* Looping through images */
+for (let image of imageFileNames) {
+    let thumbnailImage = document.createElement("img");
+    thumbnailImage.setAttribute('src', 'xxx');
+    thumbnailImage.setAttribute('alt', 'xxx');
+    thumbBar.append(thumbnailImage);
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
