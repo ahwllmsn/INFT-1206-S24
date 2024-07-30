8a28744c941ad843f333df6f44d99557fdbeeb6a
Alyssa <alyssahwllmsn@gmail.com>
2024-07-30 11:14:03 -0400

    Added proper looping and click to change image

--- Assignments/Assignment_4/Part_2/main.js
+++ Assignments/Assignment_4/Part_2/main.js
@@ -9,19 +9,23 @@ const imageFileNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jp
 
 /* Declaring the alternative text for each image file */
 const altText = {
-    pic1: "A closeup of a human eye.",
-    pic2: "A wave-like rock formation.",
-    pic3: "A closeup of white and purple flowers.",
-    pic4: "An Egyptian painting from the Tomb of Tutankhamun.",
-    pic5: "A brown moth with spotted wings on a leaf."
+    'pic1.jpg': "A closeup of a human eye.",
+    'pic2.jpg': "A wave-like rock formation.",
+    'pic3.jpg': "A closeup of white and purple flowers.",
+    'pic4.jpg': "An Egyptian painting from the Tomb of Tutankhamun.",
+    'pic5.jpg': "A brown moth with spotted wings on a leaf."
 }
 
 /* Looping through images */
 for (let image of imageFileNames) {
     let thumbnailImage = document.createElement("img");
-    thumbnailImage.setAttribute('src', 'xxx');
-    thumbnailImage.setAttribute('alt', 'xxx');
+    thumbnailImage.setAttribute('src', `images/${image}`);
+    thumbnailImage.setAttribute('alt', altText[image]);
     thumbBar.append(thumbnailImage);
+    thumbBar.addEventListener('click', (e) => {
+        displayedImage.setAttribute('src', e.target.src);
+        displayedImage.setAttribute('alt', e.target.alt);
+    });
 }
 
 const newImage = document.createElement('img');
